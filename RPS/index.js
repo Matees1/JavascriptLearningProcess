f5da94a74d3f6f108d2fdad03aed1694e90b52d3
Matees1 <maythamajam@gmail.com>
2021-06-24 20:11:42 -0400

    Started working on the javascript portion of the project

--- RPS/index.js
+++ RPS/index.js
@@ -1,6 +1,15 @@
 
 const startGameButton = document.getElementById("btn-startgame");
 const gameStatusH3 = document.getElementById("game-status-text");
+const pointsRatio = document.getElementById("game-status-text");
 let gameStarted = false;
 let playerPoints = 0;
 let computerPoints = 0;
+
+let startGame = function(){
+    gameStarted = true;
+    alert("Keep in mind that the score ratio is plr:cpu")
+    pointsRatio.style.visibility = "visible";
+}
+
+startGameButton.addEventListener("click", startGame);
