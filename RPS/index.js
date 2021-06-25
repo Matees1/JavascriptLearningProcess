d0f9a966198112959dc4d4dc9c8679ee4d61a9a9
Matees1 <maythamajam@gmail.com>
2021-06-24 21:50:46 -0400

    made many changes and added many things, will finish later on tomorrow or today but will stay like this for now

--- RPS/index.js
+++ RPS/index.js
@@ -14,9 +14,21 @@ let computerPoints = 0;
 let computerPick = "nothing";
 let playerPick = "nothing";
 
+let rps = ["rock", "papder", "scissors"];
+
+let chosen = false;
+
+let round = 0;
+let maxRounds = 3;
+
+let reset = function(){
+    gameStarted = false;
+    //add all the vars into here and reset them into their original value
+}
+
 let startGame = function(){
     gameStarted = true;
-    
+    round = 0;
     //Reseting the game points
     playerPoints = 0;
     computerPoints = 0;
@@ -30,12 +42,68 @@ let startGame = function(){
 
     //Hiding the "Start game" button
     startGameButton.style.visibility = "hidden";
+
+    while(round < maxRounds){
+        initRound();
+        round++;
+    }
 }
 
 //This will be the logic for the game, making a function just so
 //the startGame function will not be full of random things
 let initRound = function(){
+    // chosen = false;
 
+//computer side of things section 
+
+    //gets a random item from the rps array
+    let randomItem = rps[Math.floor(Math.random()*rps.length)];
+
+    switch(randomItem){
+        case "rock":
+            console.log("The bot chose rock!");
+            break;
+
+        case "scissors":
+            console.log("The bot chose scissors!");
+            break;
+
+        case "paper":
+            console.log("The bot chose paper!");
+            break;
+    }
+
+//computer side of things section end
+}
+
+
+
+let rockClick = function(){
+    if(chosen == false){
+        playerPick = "rock";
+        console.log(playerPick);
+        // chosen = true;
+    }
+}
+
+let scissorsClick = function(){
+    if(chosen == false){
+        playerPick = "scissors";
+        console.log(playerPick);
+        // chosen = true;
+    }
+}
+
+let paperClick = function(){
+    if(chosen == false){
+        playerPick = "paper";
+        console.log(playerPick);
+        // chosen = true;
+    }
 }
 
 startGameButton.addEventListener("click", startGame);
+document.getElementById("rock").addEventListener("click", rockClick);
+document.getElementById("scissors").addEventListener("click", scissorsClick);
+document.getElementById("paper").addEventListener("click", paperClick)
+document.getElementById("reset-btn").addEventListener("click", reset);
